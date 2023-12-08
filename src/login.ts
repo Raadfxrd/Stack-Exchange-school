a103def6ab3bffe793a50f2686be5254430d75ef
Youssef Ebrahim <youssef.ebrahim@hva.nl>
2023-12-08 17:17:32 +0100

    login page bijgewerkt TS

--- src/login.ts
+++ src/login.ts
@@ -5,23 +5,26 @@ import { api, session, url } from "@hboictcloud/api";
  * Deze methode wordt aangeroepen als de pagina is geladen, dat gebeurt helemaal onderin!
  */
 function setup(): void {
+    console.log(" in setup");
+    
     // Maak een actie aan voor de login knop. Als je hier op drukt wordt de code tussen de { } aangeroepen
-    document.querySelector(".login-btn")?.addEventListener("click", async () => {
+    document.querySelector("#login-form")?.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        
         // Haal de waarden uit de inputvelden met het id username en password
         const username: string = (<HTMLInputElement>document.getElementById("username")).value;
         const password: string = (<HTMLInputElement>document.getElementById("password")).value;
-
         // Roep de loginFromDatabase functie aan (op regel 50) en geef username en password mee
         try {
             const data: any = await loginFromDatabase(username, password);
-
+            console.log(data);
             if (data.length > 0) {
                 // Maak user object aan met de waarden uit de database
                 // Sla de gebruikersgegevens op in een sessie
                 session.set("user", data[0].id);
 
                 // Stuur de gebruiker door naar de homepagina
-                url.redirect("wwwroot/index.html");
+                url.redirect("index.html");
             } else {
                 // Als de gebruiker niet bestaat, geef melding aan gebruiker door in de css (bootstrap) de display op block te zetten
                 document.getElementsByClassName("alert-danger")[0].setAttribute("style", "display: block");
@@ -32,7 +35,7 @@ function setup(): void {
             }
         } catch (error) {
             // als het niet lukt de data op te halen, geef de gebruiker een foutmelding
-            console.log("Fout bij inloggen");
+            console.log(error);
         }
     });
 }
@@ -46,14 +49,20 @@ function setup(): void {
 async function loginFromDatabase(username: string, password: string): Promise<Array<any> | undefined> {
     // proberen de data op te halen uit de database
     try {
+        
+        
         const data: any = await api.queryDatabase(
             "SELECT id FROM user WHERE username = ? AND password = ?",
             username,
             password
         );
 
+        console.log(data);
+        
         return data;
     } catch (error) {
+        console.log(error);
+        
         // als het niet lukt de data op te halen, geef een lege array terug
         return [];
     }
