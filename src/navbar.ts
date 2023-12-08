bdcd33e5c90a602fa27bba599ea9aab6cee88656
Youssef Ebrahim <youssef.ebrahim@hva.nl>
2023-12-08 16:33:28 +0100

    .

--- src/navbar.ts
+++ src/navbar.ts
@@ -1,6 +1,6 @@
 import "./config";
 import { utils } from "@hboictcloud/api";
 
-]const parsedHtml: NodeList = await utils.fetchAndParseHtml("/assets/html/navbar.html");
+const parsedHtml: NodeList = await utils.fetchAndParseHtml("/assets/html/navbar.html");
 
 document.body.insertBefore(parsedHtml[0], document.body.firstChild);
