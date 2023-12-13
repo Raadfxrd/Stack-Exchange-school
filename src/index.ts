1f9345021d197330073751ca5d700ba8f741b1ac
Raadfxrd <borysbabas@gmail.com>
2023-12-13 17:23:04 +0100

    Verwijderen oude files

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import "./config";
-import { session } from "@hboictcloud/api";
-
-// Maak een actie aan voor de logout knop. Als je hier op drukt wordt de logout functie aangeroepen
-document.querySelector(".logout-btn")?.addEventListener("click", logout);
-function logout(): void {
-    // Verwijder de sessies
-    session.remove("user");}
