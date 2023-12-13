226a6253808d49152f85e39e39c708fd95233c4c
Raadfxrd <borysbabas@gmail.com>
2023-12-13 17:28:31 +0100

    Indent fixes

--- src/navbar.ts
+++ src/navbar.ts
@@ -5,8 +5,7 @@ const parsedHtml: NodeList = await utils.fetchAndParseHtml("/assets/html/navbar.
 
 document.body.insertBefore(parsedHtml[0], document.body.firstChild);
 
-
-const loggedIn:number = session.get("user");
+const loggedIn: number = session.get("user");
 
 // Doordat auto increment geen 0 kan zijn, kan je dit zo gebruiken
 if (loggedIn) {
@@ -14,30 +13,27 @@ if (loggedIn) {
     const registerBtn: Node = buttons[1];
     const loginBtn: Node = buttons[0];
 
-    // Find the login/logout button by its HTML link    
+    // Find the login/logout button by its HTML link
     // Change the Login text to "Logout"
     loginBtn!.textContent = "Logout";
-    
+
     loginBtn!.addEventListener("click", (e) => {
-        e.preventDefault(); 
+        e.preventDefault();
         localStorage.clear(); // Clear local storage data
         location.reload();
     });
     // proberen de data op te halen uit de database
-    try {            
-        const data: any = await api.queryDatabase(  "SELECT firstname, lastname FROM user2 WHERE id = ?", loggedIn    );
-            registerBtn!.textContent = data[0].firstname + " " + data[0].lastname;
+    try {
+        const data: any = await api.queryDatabase(
+            "SELECT firstname, lastname FROM user2 WHERE id = ?",
+            loggedIn
+        );
+        registerBtn!.textContent = data[0].firstname + " " + data[0].lastname;
 
-            console.log(data[0].firstname+" "+ data[0].lastname);
-    
+        console.log(data[0].firstname + " " + data[0].lastname);
     } catch (error) {
         console.log(error);
-    
+
         // als het niet lukt de data op te halen, geef een lege array terug
     }
- 
 }
-
-
-
-
