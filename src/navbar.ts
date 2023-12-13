f982f66844d78cef6841d8049fa65f22db3cb044
Raadfxrd <borysbabas@gmail.com>
2023-12-13 16:27:00 +0100

    Attempt to remove sidebar from login and register, to no avail

--- src/navbar.ts
+++ src/navbar.ts
@@ -4,3 +4,4 @@ import { utils } from "@hboictcloud/api";
 const parsedHtml: NodeList = await utils.fetchAndParseHtml("/assets/html/navbar.html");
 
 document.body.insertBefore(parsedHtml[0], document.body.firstChild);
+
